6e9653e4db763a0f57d24fab332da1a710a6dad2
sandilyan-parimalam <56201256+sandilyan-parimalam@users.noreply.github.com>
2023-11-15 15:40:10 -0500

    Create variables.tf

---
+++ modules/eks/variables.tf
@@ -0,0 +1,34 @@
+variable "dev_web_eks_iam_role" {
+  default = "dev_web_eks_iam_role"
+}
+variable "dev_web_eks_iam_policy_attachment" {
+  default = "dev_web_eks_iam_policy_attachment"
+}
+variable "dev_web_eks_iam_policy_attachment1" {
+  default = "dev_web_eks_iam_policy_attachment1"
+
+}
+variable "dev_web_eks_node_iam_role" {
+  default = "dev_web_eks_node_iam_role"
+
+}
+variable "dev_web_eks_node_iam_policy_attachment" {
+  default = "dev_web_eks_node_iam_policy_attachment"
+}
+variable "dev_web_eks_node_iam_policy_attachment1" {
+  default = "dev_web_eks_node_iam_policy_attachment1"
+}
+variable "dev_web_eks_node_iam_policy_attachment2" {
+  default = "dev_web_eks_node_iam_policy_attachment2"
+}
+variable "dev_web_eks_node_iam_policy_attachment3" {
+  default = "dev_web_eks_node_iam_policy_attachment3"
+}
+
+variable "dev_web_eks_cluster" {
+  default = "dev_web_eks_cluster"
+}
+
+variable "dev_web_eks_node_group" {
+  default = "dev_web_eks_node_group"
+}
