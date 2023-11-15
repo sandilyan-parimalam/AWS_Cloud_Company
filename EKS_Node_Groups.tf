df56429c5150d2b1190a00bdcfca049ea0c59734
sandilyan-parimalam <56201256+sandilyan-parimalam@users.noreply.github.com>
2023-11-15 04:53:54 -0500

    Update EKS_Node_Groups.tf

--- EKS_Node_Groups.tf
+++ EKS_Node_Groups.tf
@@ -14,8 +14,8 @@ resource "aws_eks_node_group" "dev_web_eks_node_group" {
 
 
   scaling_config {
-    desired_size = "2"
-    min_size     = "2"
+    desired_size = "1"
+    min_size     = "1"
     max_size     = "3"
   }
 
@@ -36,3 +36,27 @@ resource "aws_eks_node_group" "dev_web_eks_node_group" {
   }
 
 }
+
+resource "null_resource" "wait_for_lb_ip" {
+  triggers = {
+    cluster_name = aws_eks_cluster.dev_web_eks_cluster.name
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      # Wait for the Load Balancer's public IP to be available
+      lb_public_ip=""
+      retries=0
+      max_retries=30
+      while [ -z "$lb_public_ip" ]; do
+          lb_public_ip=$(kubectl get services -n dev -o=jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')
+          [ -z "$lb_public_ip" ] && sleep 10 && retries=$((retries+1))
+          [ "$retries" -ge "$max_retries" ] && echo "Error: Load Balancer IP not available after $max_retries retries" && exit 1
+      done
+
+      # Print the Load Balancer IP
+      echo "Load Balancer IP: $lb_public_ip"
+    EOT
+  }
+}
+
