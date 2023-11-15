f7c94e55c34e798d2623ceb10c033aa9c19cf37e
sandilyan-parimalam <56201256+sandilyan-parimalam@users.noreply.github.com>
2023-11-15 04:55:44 -0500

    Update EKS_Node_Groups.tf

--- EKS_Node_Groups.tf
+++ EKS_Node_Groups.tf
@@ -48,14 +48,14 @@ resource "null_resource" "wait_for_lb_ip" {
       lb_public_ip=""
       retries=0
       max_retries=30
-      while [ -z "$lb_public_ip" ]; do
-          lb_public_ip=$(kubectl get services -n dev -o=jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')
-          [ -z "$lb_public_ip" ] && sleep 10 && retries=$((retries+1))
-          [ "$retries" -ge "$max_retries" ] && echo "Error: Load Balancer IP not available after $max_retries retries" && exit 1
+      while [ -z "\$lb_public_ip" ]; do
+          lb_public_ip=\$(kubectl get services -n dev -o=jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')
+          [ -z "\$lb_public_ip" ] && sleep 10 && retries=\$((retries+1))
+          [ "\$retries" -ge "\$max_retries" ] && echo "Error: Load Balancer IP not available after \$max_retries retries" && exit 1
       done
 
       # Print the Load Balancer IP
-      echo "Load Balancer IP: $lb_public_ip"
+      echo "Load Balancer IP: \$lb_public_ip"
     EOT
   }
 }
