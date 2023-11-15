a58a2273495adeedd0e24a3795f86c9e86a6c215
sandilyan <sandilyan.parimalam@gmail.com>
2023-11-15 18:54:02 -0500

    TS

--- modules/eks/EKS_Node_Groups.tf
+++ modules/eks/EKS_Node_Groups.tf
@@ -1,22 +1,20 @@
 resource "aws_eks_node_group" "dev_web_eks_node_group" {
   node_group_name = var.dev_web_eks_node_group
   cluster_name    = aws_eks_cluster.dev_web_eks_cluster.name
-  instance_types  = [var.web_server_instance_type, ]
+  instance_types  = [var.web_server_instance_type]
   remote_access {
     ec2_ssh_key = var.web_server_instance_key_name
   }
-
 
   subnet_ids = [
     module.vpc.dev_web_subnet_id,
     module.vpc.dev_web_subnet_1_id,
   ]
 
-
   scaling_config {
-    desired_size = "1"
-    min_size     = "1"
-    max_size     = "2"
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
   }
 
   update_config {
@@ -24,20 +22,21 @@ resource "aws_eks_node_group" "dev_web_eks_node_group" {
   }
 
   capacity_type = "ON_DEMAND"
-  #node_group_name_prefix = "dev_web_eks_node_"
   node_role_arn = aws_iam_role.dev_web_eks_node_iam_role.arn
   depends_on = [
     aws_iam_role.dev_web_eks_node_iam_role,
   ]
+}
 
+resource "kubernetes_manifest" "dev_web_deployment" {
+  manifest = file("${path.module}/K8_Manifests/dev_web_manifest.yaml")
 
-  provisioner "local-exec" {
-    command = "~/kubectl apply -n dev -f K8_Manifests/Dev_Web_Manifest.yaml"
-  }
-
+  depends_on = [aws_eks_node_group.dev_web_eks_node_group]
 }
 
 resource "null_resource" "wait_for_lb_ip" {
+  depends_on = [kubernetes_manifest.dev_web_deployment]
+
   triggers = {
     cluster_name = aws_eks_cluster.dev_web_eks_cluster.name
   }
@@ -48,7 +47,7 @@ resource "null_resource" "wait_for_lb_ip" {
       max_retries=30
       lb_public_ip=""
       until [ "\$retries" -ge "\$max_retries" ]; do
-        lb_public_ip=\$(kubectl get services -n dev -o=jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')
+        lb_public_ip=\$(~/kubectl get services -n dev -o=jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')
         [ -n "\$lb_public_ip" ] && break
         retries=\$((retries+1))
         sleep 10
@@ -58,4 +57,3 @@ resource "null_resource" "wait_for_lb_ip" {
     EOT
   }
 }
-
