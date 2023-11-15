fdf4e1d7d691bdfa5f78ca7e9aac8737614bb98c
sandilyan <sandilyan.parimalam@gmail.com>
2023-11-15 15:27:58 -0500

    moduling

--- Internet_Gateway.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "dev_web_igw" {
-  vpc_id = aws_vpc.dev_vpc.id
-  tags = {
-    Name = var.internet_gateway_name
-  }
-}
--- VPC.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "dev_vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = var.vpc_name
-  }
-}
