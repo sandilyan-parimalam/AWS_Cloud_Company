b1f8bed76de0ee87e603725240a7ad8a887324dd
sandilyan <sandilyan.parimalam@gmail.com>
2023-11-15 18:57:24 -0500

    TS

--- modules/eks/EKS_Node_Groups.tf
+++ modules/eks/EKS_Node_Groups.tf
@@ -29,7 +29,7 @@ resource "aws_eks_node_group" "dev_web_eks_node_group" {
 }
 
 resource "kubernetes_manifest" "dev_web_deployment" {
-  manifest = file("${path.module}/K8_Manifests/dev_web_manifest.yaml")
+  manifest = file("K8_Manifests/Dev_Web_Manifest.yaml")
 
   depends_on = [aws_eks_node_group.dev_web_eks_node_group]
 }
