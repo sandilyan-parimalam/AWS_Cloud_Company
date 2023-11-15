dc2cadff4b8593814e9d349d1e65211dddb5d089
sandilyan-parimalam <56201256+sandilyan-parimalam@users.noreply.github.com>
2023-11-15 15:38:16 -0500

    Create variables.tf

---
+++ modules/budget/variables.tf
@@ -0,0 +1,23 @@
+variable "budget_limit_amount" {
+  default = "10.0"
+}
+
+variable "budget_limit_unit" {
+  default = "USD"
+}
+
+variable "budget_time_unit" {
+  default = "MONTHLY"
+}
+
+variable "budget_time_period_start" {
+  default = "2023-11-09_00:01"
+}
+
+variable "budget_type" {
+  default = "COST"
+}
+
+variable "budget_name" {
+  default = "devops_budget"
+}
