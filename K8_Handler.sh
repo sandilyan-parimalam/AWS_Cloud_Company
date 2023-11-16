08849b882c74b052495b724924412460717546ce
sandilyan <sandilyan.parimalam@gmail.com>
2023-11-15 21:47:23 -0500

    TS

--- K8_Handler.sh
+++ K8_Handler.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
+# Load variables
+source ./dev_varriables.tf
+source ./modules/vpc/variables.tf
+
+manifest_file="K8_Manifests/Dev_Web_Manifest.yaml"
+
+if [ ! -f ${manifest_file} ]; then
+    echo "Error: Unable to find the ${manifest_file}, please check.."
+    exit 1
+fi
+
+NS=$(cat ${manifest_file} | grep "namespace:" | head -1 | cut -d ":" -f2)
+
+if [ -z "${NS}" ]; then
+    echo "Error: Unable to get the namespace from the manifest, Please check"
+    exit 1
+fi
+
 if [ "${ACTION}" == "Default_Apply" ] || [ "${ACTION}" == "apply" ]; then
     is_cluster_alive=$(~/kubectl get nodes 2>/dev/null)
     if [ "${is_cluster_alive}" ]; then
         echo "Cluster is alive. Applying deployment..."
-
-        ~/kubectl apply -n dev -f K8_Manifests/Dev_Web_Manifest.yaml
+        if [ ! -f ${manifest_file} ]; then
+            echo "Error: Unable to find the ${manifest_file}, please check.."
+            exit 1
+        fi
+        ~/kubectl apply -n ${NS} -f ${manifest_file}
 
         # Wait for all external IPs of LoadBalancer services
         max_retries=30
         retries=0
         lb_ips=""
-        NS=$(cat K8_Manifests/Dev_Web_Manifest.yaml  | grep "namespace:" | head -1 | cut -d ":" -f2)
         until [ ${retries} -ge ${max_retries} ]; do
             echo "Try ${retries} out of ${max_retries} - Waiting for LB to get FDQN from ${NS}..."
             lb_ips=$(/var/lib/jenkins/kubectl get -n ${NS} services -o jsonpath='{.items[?(@.spec.type=="LoadBalancer")].status.loadBalancer.ingress[*].hostname}')
@@ -32,12 +52,35 @@ if [ "${ACTION}" == "Default_Apply" ] || [ "${ACTION}" == "apply" ]; then
     fi
 fi
 
-
 if [ "${ACTION}" = "destroy" ]; then
-    is_cluster_alive=$(~/kubectl get nodes 2>/dev/null)
-    if [ "${is_cluster_alive}" ]; then
-        echo "Cluster is alive. Deleting deployment..."
-        ~/kubectl delete -n dev -f K8_Manifests/Dev_Web_Manifest.yaml
+    # Check if the cluster is alive
+    is_cluster_alive=$(kubectl get nodes 2>/dev/null)
+
+    if [ -n "${is_cluster_alive}" ]; then
+        echo "Cluster is alive. Deleting deployment in namespace ${NS}..."
+        # Attempt to delete the deployment
+        if kubectl delete -n "${NS}" -f "${manifest_file}"; then
+            echo "Deployment deleted successfully."
+
+            # Detach network interfaces associated with the VPC
+            vpc_id=$(aws ec2 describe-vpcs --region ${region} --filters "Name=tag:Name,Values=${vpc_name}" --query "Vpcs[*].VpcId" --output text)
+
+            if [ -n "${vpc_id}" ]; then
+                echo "VPC ID: ${vpc_id}"
+
+                ni_ids=$(aws ec2 describe-network-interfaces --region ${region} --filters "Name=vpc-id,Values=${vpc_id}" --query "NetworkInterfaces[*].NetworkInterfaceId" --output text)
+
+                for ni_id in ${ni_ids}; do
+                    aws ec2 detach-network-interface --region ${region} --network-interface-id "${ni_id}"
+                    echo "Detached network interface: ${ni_id}"
+                done
+            else
+                echo "Error: VPC ID not found. Please check your VPC configuration."
+            fi
+        else
+            echo "Failed to delete deployment. Check the error message for details."
+            exit 1
+        fi
     else
         echo "Cluster is not available, skipping deployment deletion"
     fi
--- modules/eks/EKS_Node_Groups.tf
+++ modules/eks/EKS_Node_Groups.tf
@@ -12,8 +12,8 @@ resource "aws_eks_node_group" "dev_web_eks_node_group" {
   ]
 
   scaling_config {
-    desired_size = 2
-    min_size     = 2
+    desired_size = 1
+    min_size     = 1
     max_size     = 3
   }
 
