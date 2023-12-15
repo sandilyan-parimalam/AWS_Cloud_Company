7639682b88c169538bc89e7a7c22d28c20a2646d
sandilyan-parimalam <56201256+sandilyan-parimalam@users.noreply.github.com>
2023-12-14 21:52:42 -0600

    Create deploy.tf

---
+++ modules/eks/deploy.tf
@@ -0,0 +1,69 @@
+provider "kubernetes" {
+  host                   = aws_eks_cluster.dev_web_eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.dev_web_eks_cluster.certificate_authority.0.data)
+  token                  = aws_eks_cluster.dev_web_eks_cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "example" {
+  metadata {
+    name = "terraform-example"
+  }
+  spec {
+    selector = {
+      test = "MyExampleApp"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
