00f82d668253f61d041b23d1a793592fb71d3d1e
sandilyan-parimalam <56201256+sandilyan-parimalam@users.noreply.github.com>
2023-12-14 22:00:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ module "vpc" {
 module "budget" {
   source = "./modules/budget"
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
 module "eks" {
   source = "./modules/eks"
   region = var.region
