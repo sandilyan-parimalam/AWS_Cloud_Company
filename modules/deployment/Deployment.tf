c7667e2c13f48a9009dbbcb2da698478ceb437d1
sandilyan-parimalam <sandilyan.parimalam@gmail.com>
2023-12-16 14:14:19 -0600

    TR

--- modules/deployment/Deployment.tf
+++ modules/deployment/Deployment.tf
@@ -1,5 +1,5 @@
 resource "kubernetes_manifest" "dev_web_manifest" {
-  manifest = file("K8_Manifests/Dev_Web_Manifest.yaml")
+  manifest = yamldecode(file("K8_Manifests/Dev_Web_Manifest.yaml"))
 
   # Apply the manifest only when the workspace is "AWS_Cloud_Company"
   count = terraform.workspace == "AWS_Cloud_Company" ? 1 : 0
