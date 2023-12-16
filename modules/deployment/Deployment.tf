921d20aa0c1962ec44ffead2ea1abd38f7085460
sandilyan-parimalam <sandilyan.parimalam@gmail.com>
2023-12-16 15:09:30 -0600

    TR

--- modules/deployment/Deployment.tf
+++ modules/deployment/Deployment.tf
@@ -51,3 +51,7 @@ resource "kubernetes_service" "nginx_service" {
     type = "LoadBalancer"
   }
 }
+
+output "load_balancer_url" {
+  value = "${kubernetes_service.nginx_service.load_balancer_ingress[0].ip}:${kubernetes_service.nginx_service.ports[0].node_port}"
+}
