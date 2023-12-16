38b057eede86ef7dcb2e6166d9e6bdc332eef8e4
sandilyan-parimalam <sandilyan.parimalam@gmail.com>
2023-12-16 15:20:13 -0600

    TR

--- modules/deployment/Deployment.tf
+++ modules/deployment/Deployment.tf
@@ -53,5 +53,5 @@ resource "kubernetes_service" "nginx_service" {
 }
 
 output "load_balancer_spec" {
-  value = kubernetes_service.nginx_service.load_balancer_ingress[0].ip
+  value = try(kubernetes_service.nginx_service.load_balancer[0].ingress[0].ip, "Load balancer IP not available")
 }
