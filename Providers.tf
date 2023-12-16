c4a0287189e2a37d6c42f097720fece5208d65fa
sandilyan-parimalam <sandilyan.parimalam@gmail.com>
2023-12-15 20:46:00 -0600

    TR

--- Providers.tf
+++ Providers.tf
@@ -16,4 +16,10 @@ provider "aws" {
 
 provider "kubernetes" {
   host = module.eks.cluster_enpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.dev_web_eks_cluster.cluster_ca_certificate)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.dev_web_eks_cluster.name]
+    command     = "aws"
+  }
 }
