9b8b3f9609fc6a7bdb75d12aa0c361185ffd7d8f
sandilyan-parimalam <sandilyan.parimalam@gmail.com>
2023-12-16 14:17:37 -0600

    TR

--- modules/deployment/Deployment.tf
+++ modules/deployment/Deployment.tf
@@ -1,14 +1,11 @@
 locals {
   yaml_content = file("K8_Manifests/Dev_Web_Manifest.yaml")
+  yaml_decoded  = jsondecode(local.yaml_content)
 }
 
 resource "kubernetes_manifest" "dev_web_manifest" {
-  manifest = local.yaml_content
+  manifest = local.yaml_decoded
 
   # Apply the manifest only when the workspace is "AWS_Cloud_Company"
   count = terraform.workspace == "AWS_Cloud_Company" ? 1 : 0
-}
-
-output "decoded_yaml" {
-  value = kubernetes_manifest.dev_web_manifest
 }
